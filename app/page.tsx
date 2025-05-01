be33e6fdecd6eb08a86469f487c9287e15c025d2
cutterя <sofagucci228@gmail.com>
2025-05-01 17:01:44 +0500

    global css finally worked

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import  { Button } from '@/components/ui/button'
+import "./globals.css";
+
+
 export default function Home() {
-	return <h1>HEllo</h1>;
+	return (
+        <div>
+            <h1>HEllo</h1>
+            <Button>CLick</Button>
+        </div>
+    );
 }
